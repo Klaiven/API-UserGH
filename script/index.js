82a62624980a7e7d4b6dd9f17ffcefa06d7864e6
klaiven <klaivencastro@gmail.com>
2023-09-28 01:17:30 -0300

    atualização final feedback usuario, falta responsividade

--- script/index.js
+++ script/index.js
@@ -5,54 +5,66 @@ let main = document.getElementById('conteudo');
 function pesquisar() {
     const userName = user.value; // Obtém o valor do campo de entrada
 
-    if (userName) { // Verifica se o nome de usuário não está vazio
-        const perfil = `<section>
-            <img src="" alt="User Image" id="img">
-            <aside>
-                <p id="login">User</p>
-                <div class="seg">
-                    <p> <span id="followers"></span> Seguidores</p>
-                    <p> <span id="following"></span> Seguindo</p>
-                </div>
-                <p id="gen">Descrição: <span id="descricao"></span></p>
-            </aside>
-        </section>`;
+    fetch('https://api.github.com/users/' + userName)
+        .then(function (respostaServidor) {
+            return respostaServidor.json();
+        })
+        .then(function (respostaConvertida) {
+            console.log(respostaConvertida);
 
-        fetch('https://api.github.com/users/' + userName)
-            .then(function (respostaServidor) {
-                return respostaServidor.json();
-            })
-            .then(function (respostaConvertida) {
-                console.log(respostaConvertida);
+            let avatar = respostaConvertida.avatar_url;
 
-                let avatar = respostaConvertida.avatar_url;
+            if(respostaConvertida.login != undefined){
 
-                // Atualize os elementos no DOM com os dados da API
-                    if(respostaConvertida.login != undefined){
-                        document.getElementById('login').innerHTML = respostaConvertida.login;
-                        document.getElementById('followers').innerHTML = respostaConvertida.followers;
-                        document.getElementById('following').innerHTML = respostaConvertida.following;
-                        document.getElementById('img').src = avatar;
-                            if(respostaConvertida.bio != null && respostaConvertida.bio != "" && respostaConvertida != " "){
-                                document.getElementById('descricao').innerHTML = respostaConvertida.bio;
-                            }else{
-                                document.getElementById('descricao').innerHTML = "* Sem Descrição *";
-                            }
+                let errouser = document.getElementById('user');
+                errouser.classList.remove('nouser');
+                let errobtn = document.getElementById('btn');
+                errobtn.classList.remove('nouser');
+
+                const perfil = `
+                <section>
+                    <img src="" alt="User Image" id="img">
+                    <aside>
+                        <p id="login">User</p>
+                        <div class="seg">
+                            <p> <span id="followers"></span> Seguidores</p>
+                            <p> <span id="following"></span> Seguindo</p>
+                        </div>
+                        <p id="gen">Descrição: <span id="descricao"></span></p>
+                    </aside>
+                </section>`;
+
+            main.innerHTML = perfil;
+
+                document.getElementById('login').innerHTML = respostaConvertida.login;
+                document.getElementById('followers').innerHTML = respostaConvertida.followers;
+                document.getElementById('following').innerHTML = respostaConvertida.following;
+                document.getElementById('img').src = avatar;
+                    if(respostaConvertida.bio != null && respostaConvertida.bio != "" && respostaConvertida != " "){
+                        document.getElementById('descricao').innerHTML = respostaConvertida.bio;
                     }else{
-                        let errouser = document.getElementById('user');
-                        errouser.classList.add('nouser');
-                        let errobtn = document.getElementById('btn');
-                        errobtn.classList.add('nouser');
+                        document.getElementById('descricao').innerHTML = "* Sem Descrição *";
                     }
-            })
-            .catch(function (error) {
-                console.error("Erro na requisição: " + error);
-            });
+            }else{
 
-        main.innerHTML = perfil;
+                let errouser = document.getElementById('user');
+                errouser.classList.add('nouser');
+                let errobtn = document.getElementById('btn');
+                errobtn.classList.add('nouser');
 
-    }
+                const inexistente = `
+                <div class="alerterro">
+                    <p>Usuário Inexistente</p>
+                </div>
+                <div class="error">
+                    <ion-icon name="skull-outline"></ion-icon>
+                </div>`;
+
+            main.innerHTML = inexistente;
+            }
+        })     
 }
+
 
 
 
